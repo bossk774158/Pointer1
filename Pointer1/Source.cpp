645b9e7ee8ddf8535a462e569c8b3af82f411e76
Kittiphon Singchom <bosszakittiphon@hotmail.com>
2020-09-10 21:20:31 +0700

    Add project files.

---
+++ Pointer1/Source.cpp
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int x;
+	printf("Enter your height:");
+	scanf("%d", &x);
+	if (x < 0)
+	{
+		printf("Error");
+		}
+	else if (x > 120 && x < 130)
+	{
+		printf("Is this a human or a dwarf?");
+	}
+	else if (x > 131 && x < 140)
+	{
+		printf("You're extreamly short");
+	}
+	else if (x > 141 && x < 150)
+	{
+		printf("You're short");
+	}
+	else if (x > 151 && x < 160)
+	{
+		printf("You're average");
+	}
+	else if (x > 161 && x < 170)
+	{
+		printf("You're quite tall");
+	}
+	else if (x > 171 && x < 180)
+	{
+		printf("You're tall");
+	}
+	else if (x > 180 && x <190)
+	{
+		printf("You're very tall");
+	}
+	else if(x > 191 && x < 200)
+	{
+		printf("You're NBA Player!!");
+	}
+	else if(x>300)
+	{
+		printf("You're jinn!!");
+	}
+	return 0;
+}
